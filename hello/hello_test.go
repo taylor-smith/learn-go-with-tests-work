c16e4a7143decc3af626d81c6f4d18e83db8b500
Taylor Smith <tws@Taylors-MBP.local>
2020-12-03 10:35:47 -0800

    init commit, hello...sums

---
+++ hello/hello_test.go
@@ -0,0 +1,40 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Taylor", "")
+		want := "Hello, Taylor"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Taylor", "Spanish")
+		want := "Hola, Taylor"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Taylor", "French")
+		want := "Bonjour, Taylor"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
---
+++ iteration/iteration.go
@@ -0,0 +1,11 @@
+package iteration
+
+const repeatCount = 5
+
+func Repeat(character string, times int) string {
+	var repeated string
+	for i := 0; i < times; i++ {
+		repeated += character
+	}
+	return repeated
+}
